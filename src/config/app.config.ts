3452cd4980a3a8dc572d2d2d1018d1dc5f0ee2ea
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-09-01 17:56:37 -0500

    Screen login es agregada

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core'
 import { provideRouter } from '@angular/router'
 
-import { routes } from '@routes/app.routes'
+import { appRoutes } from '@routes/app.routes'
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+	providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes)],
 }
