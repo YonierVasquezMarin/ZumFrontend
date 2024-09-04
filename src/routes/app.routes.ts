6fc30881af2b7e04dee8018bbfdf24425298bd46
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-09-03 22:55:36 -0500

    Esquema del dashboard iniciado

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -5,4 +5,8 @@ export const appRoutes: Routes = [
 		path: '',
 		loadChildren: () => import('@routes/auth.routes').then((m) => m.authRoutes),
 	},
+	{
+		path: 'dashboard',
+		loadChildren: () => import('@routes/dashboard.routes').then((m) => m.dashboardRoutes),
+	},
 ]
---
+++ src/routes/dashboard.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router'
+import { DashboardComponent } from '@screens/dashboard/dashboard/dashboard.component'
+import { EventsComponent } from '@screens/events/events/events.component'
+
+export const dashboardRoutes: Routes = [
+	{
+		path: '',
+		component: DashboardComponent,
+		children: [
+			{
+				path: '',
+				redirectTo: 'events',
+				pathMatch: 'full',
+			},
+			{
+				path: 'events',
+				component: EventsComponent,
+			},
+		],
+	},
+]
