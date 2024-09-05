ddf3c15ed40105e273e51d588a410fa700bf0929
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-09-05 15:26:40 -0500

    Header component es maquetado correctamente

--- src/screens/dashboard/header/header.component.ts
+++ src/screens/dashboard/header/header.component.ts
@@ -1,9 +1,10 @@
+import { IconComponent } from '@common-components/icon/icon.component'
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [IconComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
