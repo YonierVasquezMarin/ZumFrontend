3a5fbae84e7517d5e9b59e5a62086e59c2c2f1b8
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-09-04 21:42:59 -0500

    Icono recibe nuevo tipo de parametro

--- src/components/common/icon/icon.component.ts
+++ src/components/common/icon/icon.component.ts
@@ -10,7 +10,17 @@ import { IconName } from '@fortawesome/fontawesome-common-types'
 	styleUrl: './icon.component.scss',
 })
 export class IconComponent {
-	@Input({ required: true }) name!: IconName
-	@Input() size: number = 32
+	@Input({ required: true }) name!: IconName | IconName[]
+	@Input() size: number = 16
 	@Input() color: string = 'white'
+
+	get iconClasses() {
+		let classes = ['fas']
+		if (Array.isArray(this.name)) {
+			classes.push(...this.name.map((name) => 'fa-' + name))
+		} else {
+			classes.push('fa-' + this.name)
+		}
+		return classes
+	}
 }
