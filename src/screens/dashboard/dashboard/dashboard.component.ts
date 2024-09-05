6743ef725db8f4267b3f8d4a0ec94e16fd1a2436
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-09-05 11:36:37 -0500

    Componentes side bar y header son agregados

--- src/screens/dashboard/dashboard/dashboard.component.ts
+++ src/screens/dashboard/dashboard/dashboard.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from '@screens/dashboard/side-bar/side-bar.component'
+import { HeaderComponent } from '@screens/dashboard/header/header.component'
+import { RouterOutlet } from '@angular/router'
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+	selector: 'app-dashboard',
+	standalone: true,
+	imports: [RouterOutlet, HeaderComponent, SideBarComponent],
+	templateUrl: './dashboard.component.html',
+	styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
