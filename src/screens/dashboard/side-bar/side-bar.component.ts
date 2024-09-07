b4695d47356d0bdad1293c70888c65571295276c
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-09-07 15:54:52 -0500

    Menu list es agregado a side bar

--- src/screens/dashboard/side-bar/side-bar.component.ts
+++ src/screens/dashboard/side-bar/side-bar.component.ts
@@ -1,13 +1,12 @@
-import { UserInfoComponent } from '@specific-components/dashboard/user-info/user-info.component';
-import { Component } from '@angular/core';
+import { UserInfoComponent } from '@specific-components/dashboard/user-info/user-info.component'
+import { MenuListComponent } from '@specific-components/dashboard/menu-list/menu-list.component'
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-side-bar',
-  standalone: true,
-  imports: [UserInfoComponent],
-  templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.scss'
+	selector: 'app-side-bar',
+	standalone: true,
+	imports: [UserInfoComponent, MenuListComponent],
+	templateUrl: './side-bar.component.html',
+	styleUrl: './side-bar.component.scss',
 })
-export class SideBarComponent {
-
-}
+export class SideBarComponent {}
