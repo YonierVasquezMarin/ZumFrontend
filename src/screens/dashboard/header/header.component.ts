e05fb38d67499e21ad581e3b767c88f5a5a6e52e
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-09-07 17:45:51 -0500

    Sidebar es configurado con header

--- src/screens/dashboard/header/header.component.ts
+++ src/screens/dashboard/header/header.component.ts
@@ -1,13 +1,36 @@
+import { SideBarComponent } from '@screens/dashboard/side-bar/side-bar.component'
 import { IconComponent } from '@common-components/icon/icon.component'
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core'
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  imports: [IconComponent],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+	selector: 'app-header',
+	standalone: true,
+	imports: [IconComponent, SideBarComponent],
+	templateUrl: './header.component.html',
+	styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+	sideBarIsOpen = false
 
+	constructor(private elementRef: ElementRef) {}
+
+	toggleSideBar() {
+		this.sideBarIsOpen = !this.sideBarIsOpen
+	}
+
+	@HostListener('document:click', ['$event'])
+	onDocumentClick(event: Event): void {
+		if (this.sidebarIsOpenAndUserTouchedOutsideOfThis(event)) {
+			this.toggleSideBar()
+		}
+	}
+
+	sidebarIsOpenAndUserTouchedOutsideOfThis(event: Event): boolean {
+		return this.sideBarIsOpen && this.sideBarParentIsTouched(event)
+	}
+
+	sideBarParentIsTouched(event: Event): boolean {
+		const targetElement = event.target as HTMLElement
+		return targetElement == targetElement.closest('app-side-bar')
+	}
 }
