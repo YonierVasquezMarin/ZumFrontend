ea30435435aae2c4a61a3916a0f455c11aeff911
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-09-07 22:29:09 -0500

    Header configurado con animacion

--- src/components/specific/dashboard/header/header.component.ts
+++ src/components/specific/dashboard/header/header.component.ts
@@ -1,19 +1,27 @@
 import { SideBarComponent } from '@specific-components/dashboard/side-bar/side-bar.component'
 import { IconComponent } from '@common-components/icon/icon.component'
-import { Component, HostListener } from '@angular/core'
+import { Component, HostListener, OnInit } from '@angular/core'
+import { NgStyle } from '@angular/common'
 
 @Component({
 	selector: 'app-header',
 	standalone: true,
-	imports: [IconComponent, SideBarComponent],
+	imports: [IconComponent, SideBarComponent, NgStyle],
 	templateUrl: './header.component.html',
 	styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 	sideBarIsOpen = false
+	sideBarStyles = {}
+	backgroundStyles = {}
+
+	ngOnInit() {
+		this.defineSideBarStyles()
+	}
 
 	toggleSideBar() {
 		this.sideBarIsOpen = !this.sideBarIsOpen
+		this.defineSideBarStyles()
 	}
 
 	@HostListener('document:click', ['$event'])
@@ -29,6 +37,45 @@ export class HeaderComponent {
 
 	sideBarParentIsTouched(event: Event): boolean {
 		const targetElement = event.target as HTMLElement
-		return targetElement == targetElement.closest('app-side-bar')
+		const targetElementIsSidebarContainer = targetElement.querySelector('app-side-bar')
+		return targetElementIsSidebarContainer !== null
+	}
+
+	defineSideBarStyles() {
+		if (this.sideBarIsOpen) {
+			this.showBackground()
+			this.showSideBarWithDelay()
+		} else {
+			this.hideSideBar()
+			this.hideBackgroundWithDelay()
+		}
+	}
+
+	showBackground() {
+		this.backgroundStyles = {
+			display: 'block',
+		}
+	}
+
+	showSideBarWithDelay() {
+		setTimeout(() => {
+			this.sideBarStyles = {
+				left: '0',
+			}
+		}, 100)
+	}
+
+	hideSideBar() {
+		this.sideBarStyles = {
+			left: '-280px',
+		}
+	}
+
+	hideBackgroundWithDelay() {
+		setTimeout(() => {
+			this.backgroundStyles = {
+				display: 'none',
+			}
+		}, 250)
 	}
 }
