db497776fc719bfcecb6ab378127f3e3df268045
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-09-07 23:00:43 -0500

    Items menu agregados

--- src/components/specific/side-bar/menu-list/menu-list.component.ts
+++ src/components/specific/side-bar/menu-list/menu-list.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { MenuItemType } from '@type/menu-item.type'
+import { Component } from '@angular/core'
+import { ButtonComponent } from '@common-components/button/button.component'
 
 @Component({
-  selector: 'app-menu-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './menu-list.component.html',
-  styleUrl: './menu-list.component.scss'
+	selector: 'app-menu-list',
+	standalone: true,
+	imports: [ButtonComponent],
+	templateUrl: './menu-list.component.html',
+	styleUrl: './menu-list.component.scss',
 })
 export class MenuListComponent {
-
+	menuItems: MenuItemType[] = [
+		{
+			name: 'Eventos',
+			icon: 'calendar-alt',
+			route: '/events',
+		},
+		{
+			name: 'Notificaciones',
+			icon: 'bell',
+			route: '/notifications',
+		}
+	]
 }
