c9b358dd3059704af86c1da14377b7bfe6a92dee
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-09-11 07:39:20 -0500

    Dto para crear evento es implementado

--- src/dtos/events.dtos.ts
+++ src/dtos/events.dtos.ts
@@ -1,27 +1,41 @@
 export interface EventResponseDto {
-     eventId: string;
-     eventName: string;
-     eventNumber: number;
-     description: string;
-     objective: string;
-     startDate: Date;
-     startTime: string;
-     endDate: Date;
-     endTime: string; 
-     country: string;
-     department: string;
-     city: string;
-     assignedBudget: number;
-     spentBudget: number; 
-     status: number;
-     createdAt: Date;
-     contractId: string;
-     contractName: string;
-     companyId: string;
-     companyName: string;
-     companyAcronym: string;
-     operatorId: string;
-     operatorName: string;  
-     operatorAcronym: string;
-  }
-  
+	eventId: string
+	eventName: string
+	eventNumber: number
+	description: string
+	objective: string
+	startDate: Date
+	startTime: string
+	endDate: Date
+	endTime: string
+	country: string
+	department: string
+	city: string
+	assignedBudget: number
+	spentBudget: number
+	status: number
+	createdAt: Date
+	contractId: string
+	contractName: string
+	companyId: string
+	companyName: string
+	companyAcronym: string
+	operatorId: string
+	operatorName: string
+	operatorAcronym: string
+}
+
+export interface CreateEventDto {
+	eventName: string
+	description: string
+	objective: string
+	startDate: Date
+	startTime: string
+	endDate: Date
+	endTime: string
+	country: string
+	department: string
+	city: string
+	assignedBudget: number
+	contractId: string
+}
