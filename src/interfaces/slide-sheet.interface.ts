b7475b23d0611b7ce3c754e1803c9c75353823cf
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-09-10 21:32:12 -0500

    Esquema slider es retirado

--- src/interfaces/slide-sheet.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface SlideSheet {
-	title: string // Cada slide tiene un título
-	checkSlide(): boolean // Método para validar si la slide es correcta
-}
--- src/screens/events/event-detail/event-detail.component.ts
+++ src/screens/events/event-detail/event-detail.component.ts
@@ -1,15 +1,10 @@
-import { EventContractComponent } from '@specific-components/event-detail/event-contract/event-contract.component'
-import { EventInfoComponent } from '@specific-components/event-detail/event-info/event-info.component'
-import { SlidesComponent } from '@common-components/slides/slides.component'
 import { Component } from '@angular/core'
 
 @Component({
 	selector: 'app-event-detail',
 	standalone: true,
-	imports: [SlidesComponent, EventInfoComponent, EventContractComponent],
+	imports: [],
 	templateUrl: './event-detail.component.html',
 	styleUrl: './event-detail.component.scss',
 })
-export class EventDetailComponent {
-	slides = [EventInfoComponent, EventContractComponent]
-}
+export class EventDetailComponent {}
