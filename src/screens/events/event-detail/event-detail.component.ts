16dbfafc0d174293054361f353cb4be19902cf6e
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2024-09-11 07:39:31 -0500

    Campos iniciales son agregados

--- src/screens/events/event-detail/event-detail.component.ts
+++ src/screens/events/event-detail/event-detail.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core'
+import { ValidationMessageComponent } from '@common-components/validation-message/validation-message.component'
+import { CreateEventDto } from '@dtos/events.dtos'
+import { Component, OnInit } from '@angular/core'
+import {
+	AbstractControl,
+	FormBuilder,
+	FormControl,
+	FormGroup,
+	ReactiveFormsModule,
+	Validators,
+} from '@angular/forms'
+import { CommonModule } from '@angular/common'
+import { ButtonComponent } from '@common-components/button/button.component'
 
 @Component({
 	selector: 'app-event-detail',
 	standalone: true,
-	imports: [],
+	imports: [CommonModule, ReactiveFormsModule, ButtonComponent, ValidationMessageComponent],
 	templateUrl: './event-detail.component.html',
 	styleUrl: './event-detail.component.scss',
 })
-export class EventDetailComponent {}
+export class EventDetailComponent implements OnInit {
+	constructor(private formBuilder: FormBuilder) {}
+
+	eventForm!: FormGroup
+
+	ngOnInit(): void {
+		this.buildForm()
+	}
+
+	buildForm() {
+		this.eventForm = this.formBuilder.group({
+			eventName: new FormControl('', [Validators.required]),
+			description: new FormControl('', [Validators.required]),
+			objective: new FormControl('', [Validators.required]),
+			startDate: new FormControl('', [Validators.required]),
+			startTime: new FormControl('', [Validators.required]),
+			endDate: new FormControl('', [Validators.required]),
+			endTime: new FormControl('', [Validators.required]),
+			country: new FormControl('', [Validators.required]),
+			department: new FormControl('', [Validators.required]),
+			city: new FormControl('', [Validators.required]),
+			assignedBudget: new FormControl('', [Validators.required]),
+			contractId: new FormControl('', [Validators.required]),
+		})
+	}
+
+	saveEvent() {
+		if (this.eventForm.invalid) {
+			this.eventForm.markAllAsTouched()
+			return
+		}
+		let event: CreateEventDto = { ...this.eventForm.value }
+		// this.eventsService.createEvent(event).subscribe({
+		// 	next: (response) => this.manageResponseCreateEvent(response),
+		// 	error: (error) => this.manageErrorCreateEvent(error),
+		// })
+	}
+
+	getControl(name: string) {
+		return this.eventForm.get(name) as AbstractControl
+	}
+}
